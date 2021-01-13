94d02ec639cd6974c5c1d1cb59dc8d49ad9b48c4
Sotiris Blad <sotblad@monetaryunit.org>
2021-01-14 01:27:59 +0200

    Update index.js

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,8 +1,8 @@
 import ethProvider from './eth_provider'
 import { ethers } from 'ethers'
 import config from 'config'
-import DAI_ABI from './abi/ERC20.json'
+import WMUE_ABI from './abi/ERC20.json'
 
-export const DAI = new ethers.Contract(config.public.contracts.DAI.address, DAI_ABI, ethProvider)
+export const WMUE = new ethers.Contract(config.public.contracts.WMUE.address, WMUE_ABI, ethProvider)
 
 export { ethProvider }
