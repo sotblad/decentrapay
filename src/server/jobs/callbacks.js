7b7a39a5e3b2131409ffb16cccb2d252c043c498
Sotiris Blad <sotblad@monetaryunit.org>
2021-01-14 02:09:07 +0200

    Update callbacks.js

--- src/server/jobs/callbacks.js
+++ src/server/jobs/callbacks.js
@@ -6,7 +6,7 @@ export async function checkPendingNotifications() {
   const invoices = await Invoices.findPaidInvoices()
   for(let invoice of invoices) {
     if (!invoice.notified && invoice.callbacks) {
-      request.post(invoice.callbacks.paid.url, {json:{token:invoice.callbacks.token,invoiceId:invoice._id,metadata:invoice.metadata}}, (err, resp) => {
+      request.post(invoice.callbacks[0].paid.url, {json:{token:invoice.callbacks.token,invoiceId:invoice._id,metadata:invoice.metadata}}, (err, resp) => {
         if (err || resp.statusCode != 200) {
           console.log(`invoice #${invoice._id}: callback error occured:`,err)
           return
